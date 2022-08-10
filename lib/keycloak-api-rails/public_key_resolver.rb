27889e244b48ac4f2bcf88648a29f4dba28ddb23
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-08-11 01:26:10 +0700

    debug

--- lib/keycloak-api-rails/public_key_resolver.rb
+++ lib/keycloak-api-rails/public_key_resolver.rb
@@ -8,7 +8,7 @@ module Keycloak
     def find_public_keys
       # JSON::JWK::Set.new(@http_client.get(@realm_id, "protocol/openid-connect/certs")["keys"])
       # OpenSSL::PKey::RSA.new("-----BEGIN PUBLIC KEY-----\n #{@http_client.get(@realm_id, '')['public_key']} \n-----END PUBLIC KEY-----\n")
-      puts @http_client.get(@realm_id, '')
+      puts @http_client.get(@realm_id, '')['public_key']
     end
   end
 end
