58e7286ee770b42c5847f88199dbcec09ac0e054
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-10-07 02:10:22 +0700

    Change some function

--- lib/keycloak-api-rails/helper.rb
+++ lib/keycloak-api-rails/helper.rb
@@ -1,69 +1,69 @@
 module Keycloak
   class Helper
-    CURRENT_USER_ID_KEY          = 'keycloak:keycloak_id'
-    CURRENT_AUTHORIZED_PARTY_KEY = 'keycloak:authorized_party'
-    CURRENT_USER_EMAIL_KEY       = 'keycloak:email'
-    CURRENT_USER_LOCALE_KEY      = 'keycloak:locale'
-    CURRENT_USER_ATTRIBUTES      = 'keycloak:attributes'
-    ROLES_KEY                    = 'keycloak:roles'
-    RESOURCE_ROLES_KEY           = 'keycloak:resource_roles'
-    TOKEN_KEY                    = 'keycloak:token'
-    QUERY_STRING_TOKEN_KEY       = 'authorizationToken'
+    CURRENT_USER_ID_KEY          = 'keycloak:keycloak_id'.freeze
+    CURRENT_AUTHORIZED_PARTY_KEY = 'keycloak:authorized_party'.freeze
+    CURRENT_USER_EMAIL_KEY       = 'keycloak:email'.freeze
+    CURRENT_USER_LOCALE_KEY      = 'keycloak:locale'.freeze
+    CURRENT_USER_ATTRIBUTES      = 'keycloak:attributes'.freeze
+    ROLES_KEY                    = 'keycloak:roles'.freeze
+    RESOURCE_ROLES_KEY           = 'keycloak:resource_roles'.freeze
+    DECODED_TOKEN_KEY            = 'keycloak:decoded_token'.freeze
+    QUERY_STRING_TOKEN_KEY       = 'authorizationToken'.freeze
 
     def self.current_user_id(env)
       env[CURRENT_USER_ID_KEY]
     end
 
-    def self.assign_current_user_id(env, token)
-      env[CURRENT_USER_ID_KEY] = token['sub']
+    def self.assign_current_user_id(env, decoded)
+      env[CURRENT_USER_ID_KEY] = decoded['sub']
     end
 
-    def self.keycloak_token(env)
-      env[TOKEN_KEY]
+    def self.decoded_token
+      env[DECODED_TOKEN_KEY]
     end
 
-    def self.assign_keycloak_token(env, token)
-      env[TOKEN_KEY] = token
+    def self.assign_decoded_token(env, decoded)
+      env[DECODED_TOKEN_KEY] = decoded
     end
 
-    def self.current_authorized_party(env)
+    def self.current_authorized_party
       env[CURRENT_AUTHORIZED_PARTY_KEY]
     end
 
-    def self.assign_current_authorized_party(env, token)
-      env[CURRENT_AUTHORIZED_PARTY_KEY] = token['azp']
+    def self.assign_current_authorized_party(env, decoded)
+      env[CURRENT_AUTHORIZED_PARTY_KEY] = decoded['azp']
     end
 
-    def self.current_user_email(env)
+    def self.current_user_email
       env[CURRENT_USER_EMAIL_KEY]
     end
 
-    def self.assign_current_user_email(env, token)
-      env[CURRENT_USER_EMAIL_KEY] = token['email']
+    def self.assign_current_user_email(env, decoded)
+      env[CURRENT_USER_EMAIL_KEY] = decoded['email']
     end
 
-    def self.current_user_locale(env)
+    def self.current_user_locale
       env[CURRENT_USER_LOCALE_KEY]
     end
 
-    def self.assign_current_user_locale(env, token)
-      env[CURRENT_USER_LOCALE_KEY] = token['locale']
+    def self.assign_current_user_locale(env, decoded)
+      env[CURRENT_USER_LOCALE_KEY] = decoded['locale']
     end
 
-    def self.current_user_roles(env)
+    def self.current_user_roles
       env[ROLES_KEY]
     end
 
-    def self.assign_realm_roles(env, token)
-      env[ROLES_KEY] = token.dig('realm_access', 'roles')
+    def self.assign_realm_roles(env, decoded)
+      env[ROLES_KEY] = decoded.dig('realm_access', 'roles')
     end
 
-    def self.current_resource_roles(env)
+    def self.current_resource_roles
       env[RESOURCE_ROLES_KEY]
     end
 
-    def self.assign_resource_roles(env, token)
-      env[RESOURCE_ROLES_KEY] = token.fetch('resource_access', {}).each_with_object({}) do |(name, resource_attributes), resource_roles|
+    def self.assign_resource_roles(env, decoded)
+      env[RESOURCE_ROLES_KEY] = decoded.fetch('resource_access', {}).each_with_object({}) do |(name, resource_attributes), resource_roles|
         resource_roles[name] = resource_attributes.fetch('roles', [])
       end
 
@@ -71,11 +71,11 @@ module Keycloak
       # env[RESOURCE_ROLES_KEY] = token.fetch('resource_access', {}).transform_values { |resource_attributes| resource_attributes.fetch('roles', []) }
     end
 
-    def self.assign_current_user_custom_attributes(env, token, attribute_names)
-      env[CURRENT_USER_ATTRIBUTES] = token.select { |key, _value| attribute_names.include?(key) }
+    def self.assign_current_user_custom_attributes(env, decoded, attribute_names)
+      env[CURRENT_USER_ATTRIBUTES] = decoded.select { |key, _value| attribute_names.include?(key) }
     end
 
-    def self.current_user_custom_attributes(env)
+    def self.current_user_custom_attributes
       env[CURRENT_USER_ATTRIBUTES]
     end
 
@@ -100,11 +100,10 @@ module Keycloak
       headers['HTTP_AUTHORIZATION']&.gsub(/^Bearer /, '') || ''
     end
 
-    def self.token_attribute(token, attribute_name)
-      decoded_token = Keycloak.service.decode_and_verify(token)
+    def self.decoded_token_attribute(attribute_name)
       attribute = decoded_token.select { |attr| attr[attribute_name] }
 
-      raise TokenError.attribute_not_found token if attribute.blank?
+      raise TokenError.attribute_not_found decoded_token if attribute.blank?
 
       attribute
     end
