1965e9745bf20a388394a08ce87d9ae9e8b480d7
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-10-06 09:28:01 +0700

    Change Behaviour

--- lib/keycloak-api-rails/service.rb
+++ lib/keycloak-api-rails/service.rb
@@ -7,25 +7,38 @@ module Keycloak
       @token_expiration_tolerance_in_seconds = Keycloak.config.token_expiration_tolerance_in_seconds
     end
 
+    # def decode_and_verify(token)
+    #   if token.nil? || token&.empty?
+    #     raise TokenError.no_token(token)
+    #   else
+    #     public_key    = @key_resolver.find_public_keys
+    #     # decoded_token = JSON::JWT.decode(token, public_key)
+    #     decoded_token = JWT.decode(token, public_key, true, { algorithm: 'RS256' })
+    #     if expired?(decoded_token)
+    #       raise TokenError.expired(token)
+    #     else
+    #       # decoded_token.verify!(public_key)
+    #       decoded_token
+    #     end
+    #   end
+    # # rescue JSON::JWT::VerificationFailed => e
+    #   # raise TokenError.verification_failed(token, e)
+    # # rescue JSON::JWK::Set::KidNotFound => e
+    #   # raise TokenError.verification_failed(token, e)
+    # # rescue JSON::JWT::InvalidFormat
+    # rescue JWT::DecodeError => e
+    #   raise TokenError.invalid_format(token, e)
+    # end
     def decode_and_verify(token)
-      if token.nil? || token&.empty?
-        raise TokenError.no_token(token)
+      public_key    = @key_resolver.find_public_keys
+      # decoded_token = JSON::JWT.decode(token, public_key)
+      decoded_token = JWT.decode(token, public_key, true, { algorithm: 'RS256' })
+      if expired?(decoded_token)
+        raise TokenError.expired(token)
       else
-        public_key    = @key_resolver.find_public_keys
-        # decoded_token = JSON::JWT.decode(token, public_key)
-        decoded_token = JWT.decode(token, public_key, true, { algorithm: 'RS256' })
-        if expired?(decoded_token)
-          raise TokenError.expired(token)
-        else
-          # decoded_token.verify!(public_key)
-          decoded_token
-        end
+        # decoded_token.verify!(public_key)
+        decoded_token
       end
-    # rescue JSON::JWT::VerificationFailed => e
-      # raise TokenError.verification_failed(token, e)
-    # rescue JSON::JWK::Set::KidNotFound => e
-      # raise TokenError.verification_failed(token, e)
-    # rescue JSON::JWT::InvalidFormat
     rescue JWT::DecodeError => e
       raise TokenError.invalid_format(token, e)
     end
