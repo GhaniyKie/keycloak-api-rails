1fe09ddc6da96483bb7f07558997fc005132465c
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-10-06 11:57:46 +0700

    Rollback

--- lib/keycloak-api-rails.rb
+++ lib/keycloak-api-rails.rb
@@ -1,22 +1,21 @@
-require "logger"
-# require "json/jwt"
-require "jwt"
-require "uri"
-require "date"
-require "net/http"
+require 'logger'
+require 'json/jwt'
+require 'jwt'
+require 'uri'
+require 'date'
+require 'net/http'
 
-require_relative "keycloak-api-rails/configuration"
-require_relative "keycloak-api-rails/http_client"
-require_relative "keycloak-api-rails/token_error"
-require_relative "keycloak-api-rails/helper"
-require_relative "keycloak-api-rails/public_key_resolver"
-require_relative "keycloak-api-rails/public_key_cached_resolver"
-require_relative "keycloak-api-rails/service"
-require_relative "keycloak-api-rails/middleware"
-require_relative "keycloak-api-rails/railtie" if defined?(Rails)
+require_relative 'keycloak-api-rails/configuration'
+require_relative 'keycloak-api-rails/http_client'
+require_relative 'keycloak-api-rails/token_error'
+require_relative 'keycloak-api-rails/helper'
+require_relative 'keycloak-api-rails/public_key_resolver'
+require_relative 'keycloak-api-rails/public_key_cached_resolver'
+require_relative 'keycloak-api-rails/service'
+require_relative 'keycloak-api-rails/middleware'
+require_relative 'keycloak-api-rails/railtie' if defined?(Rails)
 
 module Keycloak
-
   def self.configure
     yield @configuration ||= Keycloak::Configuration.new
   end
@@ -48,7 +47,7 @@ module Keycloak
       config.logger                                 = ::Logger.new(STDOUT)
       config.skip_paths                             = {}
       config.token_expiration_tolerance_in_seconds  = 10
-      config.public_key_cache_ttl                   = 86400
+      config.public_key_cache_ttl                   = 86_400
       config.custom_attributes                      = []
     end
   end
